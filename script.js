fb9b95f9b432e9c366c597484bfbce13dc7d9fbf
ClementMonard <clement.monard@hotmail.fr>
2018-06-20 14:16:17 +0200

    Modification de l'ensemble du code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var lastName;
-var firstName;
-var city;
-lastName = 'Nom : Doe';
-firstName ='Prénom : John';
-city = 'Ville : New-York';
-result = lastName+"\n"+firstName+"\n"+city;
+//déclaration des variables et assignation de leurs valeurs.
+var lastName = 'Doe';
+var firstName ='John';
+var city = 'New-York';
+result = 'Nom : ' + lastName+'\n'+'Prénom : ' + firstName+'\n'+'Ville : ' + city;
 alert(result);
