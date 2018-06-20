605dcf7ac0420b75af5a0ddb09f86f7a14038ef6
ClementMonard <clement.monard@hotmail.fr>
2018-06-20 10:43:52 +0200

    Présentation

---
+++ script.js
@@ -0,0 +1,8 @@
+var lastName;
+var firstName;
+var city;
+lastName = 'Nom : Doe';
+firstName ='Prénom : John';
+city = 'Ville : New-York';
+result = lastName+"\n"+firstName+"\n"+city;
+alert(result);
